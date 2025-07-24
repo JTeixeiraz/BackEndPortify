b5fd9cf155e1d33d0fb374090ec4e94d040d8aac
JTeixeiraz <joaopedroteixeirareis@gmail.com>
2025-07-23 22:43:17 -0300

    Commit do conteudo

---
+++ src/main/java/com/Portify/BackPortify/BackPortifyApplication.java
@@ -0,0 +1,13 @@
+package com.Portify.BackPortify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPortifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPortifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Portify/BackPortify/BackPortifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portify.BackPortify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPortifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
