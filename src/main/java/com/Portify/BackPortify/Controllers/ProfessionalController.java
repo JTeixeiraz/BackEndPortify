2bb407a14b20232bed20bb487283ecf820218220
JTeixeiraz <joaopedroteixeirareis@gmail.com>
2025-07-26 17:42:49 -0300

    Colocando o template de site pessoal com header no topo

--- src/main/java/com/Portify/BackPortify/Controllers/ProfessionalController.java
+++ src/main/java/com/Portify/BackPortify/Controllers/ProfessionalController.java
@@ -1,4 +1,19 @@
 package com.Portify.BackPortify.Controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Conceitos para lembrar:
+//@RequestBody é a notação utilizada para se referir ao corpo da requisição.
+//ResponseEntity -> É um tipo que representa toda a response http (status, corpo, header)
+
+
+@RestController
+@RequestMapping("/professional")
 public class ProfessionalController {
+    @PostMapping
+    public void teste(){
+
+    }
 }
